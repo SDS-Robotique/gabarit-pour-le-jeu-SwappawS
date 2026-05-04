a9d486828d71019bd26275740437c761a708e7c5
SwappawS <gregoires@seminaire-sherbrooke.qc.ca>
2026-05-04 20:04:35 +0000

    Chris Christodoulou - The Rain Formerly Known as Purple

--- game.js
+++ game.js
@@ -5,6 +5,10 @@ var enemy;
 var bg;
 var inv;
 var KeyW;
+var KeyA;
+var KeyD;
+var platforms;
+
 
 class Game extends Phaser.Scene {    
     constructor(){
@@ -20,6 +24,7 @@ class Game extends Phaser.Scene {
     preload(){
     this.load.image('forest', 'assets/images/forest_background.png');
     this.load.spritesheet('SlimeV', ' assets/sprites/slime1.png', { frameWidth: 192, frameHeight: 192 });
+    this.load.spritesheet('platform', 'assets/sprites/platform.png', {frameWidth:63, frameHeight:18});
     }
     
     create(data){
@@ -29,23 +34,35 @@ class Game extends Phaser.Scene {
     
     
     player = this.physics.add.sprite(480, 270, 'SlimeV');
-    player.setBounce(0.1);
+    platforms= this.add.sprite(100,270, 'platform');
+    this.physics.add.collider(player, platforms);
+    
+    player.setBounce(0.5);
     player.setCollideWorldBounds(true);
     player.setScale(1);
     player.body.setSize(58,34);
     KeyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    
-    
+    KeyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    KeyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.score = 0;
 
     }
     
     update(time, delta){
-        if(KeyW.isDown){
+        console.log(player.body.touching.down);
+        if(KeyW.isDown && player.body.touching.down){
             player.setVelocityY(-240);
         }
+            if (KeyA.isDown) {
+                player.setVelocityX(-150);
+            } 
+            else if (KeyD.isDown){
+                player.setVelocityX(150);
+            }
+            else{
+                player.setVelocityX(0);
+            }
      }
-     
 
     }
 
