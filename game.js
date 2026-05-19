e0a102f9e7c44fc6ca353cb82d98cab8815b78b7
SwappawS <gregoires@seminaire-sherbrooke.qc.ca>
2026-05-19 17:03:53 +0000

    19 mai, période et midi, uncaught type error encore un problème

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+import { Create } from "phaser";
+
 var pdv;
 var player;
 var score;
@@ -10,6 +12,8 @@ var KeyD;
 var platforms;
 var sol;
 var ennemyGroup;
+
+
 
 class Game extends Phaser.Scene {    
     constructor(){
@@ -31,63 +35,81 @@ class Game extends Phaser.Scene {
 }
     
     create(data){
-    bg = this.add.image(0,0,'forest');
-    bg.setScale(2);
-    bg.setOrigin(0, 0);
+    bg = this.add.image(0,0,'forest')
+    bg.setScale(2)
+    bg.setOrigin(0, 0)
     
     
-    player = this.physics.add.sprite(480, 270, 'SlimeV');
-    platforms= this.add.sprite(100,270, 'platform');
-    sol= this.add.sprite(470, 790, 'sol');
+    player = this.physics.add.sprite(480, 270, 'SlimeV')
+    platforms= this.add.sprite(100,270, 'platform')
+    sol= this.add.sprite(470, 790, 'sol')
     sol.setScale(3);
-    this.physics.add.collider(player, platforms);
-    this.physics.add.collider(player, sol);
+    this.physics.add.collider(player, platforms)
+    this.physics.add.collider(player, sol)
     
-    ennemy=this.physics.add.sprite(100,200, 'crane');
-    ennemy.setScale(0.2);
-    this.physics.add.collider(ennemy, platforms);
-    this.physics.add.collider(ennemy, sol);
+    ennemy=this.physics.add.sprite(100,200, 'crane')
+    ennemy.setScale(0.2)
+    this.physics.add.collider(ennemy, platforms)
+    this.physics.add.collider(ennemy, sol)
 
     
-    player.setBounce(0.5);
-    player.setCollideWorldBounds(true);
+    player.setBounce(0.5)
+    player.setCollideWorldBounds(true)
     
-    player.setScale(1);
-    player.body.setSize(58,34);
-    ennemy.setCollideWorldBounds(true);
-    this.ennemy = this.physics.add.group();
+    player.setScale(1)
+    player.body.setSize(58,34)
+    ennemy.setCollideWorldBounds(true)
+    ennemyGroup = this.physics.add.group()
     
+    this.ennemyGroup.children.each(function(item){
+        this.time.addEvent()
+        {
+            item.create()
+            {
+    delay; 3000, 
+    callback; this.spawnEnnemy, 
+    callbackScope; this,
+    loop; true
+    }
+   }
+   
+        
+    })
+
 
     ennemy.setBounce(0.5);
-    KeyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    KeyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    KeyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.score = 0;
+    KeyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+    KeyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+    KeyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+    this.score = 0
 
-    }
+}
     
     update(time, delta){
-        console.log(player.body.onFloor());
+        console.log(player.body.onFloor())
         if(KeyW.isDown && player.body.onFloor()){
-            player.setVelocityY(-240);
+            player.setVelocityY(-240)
         }
             if (KeyA.isDown) {
-                player.setVelocityX(-150);
+                player.setVelocityX(-150)
             } 
             else if (KeyD.isDown){
-                player.setVelocityX(150);
+                player.setVelocityX(150)
             }
             else{
-                player.setVelocityX(0);
+                player.setVelocityX(0)
             }
-       function spawnEnnemy(){
-       this.time.addEvent({
-        delay: 5000,                
-        callback: spawnEnnemy,             
-        loop: true                  
-    });
+       
+
+        
 }
       }
-    }
+    
+spawnEnnemy()
+{
+    if(time>3000)
+    spawnEnnemy == true
+    ennemyGroup.add(ennemy)
+}
 
 export default Game
